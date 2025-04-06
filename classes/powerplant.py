7e3f82cdfa73e3068dd02f10a85229e81936b73d
Solipa21 <up202403336@fe.up.pt>
2025-04-06 21:25:45 +0100

    terceira alteração

--- classes/powerplant.py
+++ classes/powerplant.py
@@ -13,20 +13,20 @@ class Powerplant(Gclass):
     pos = 0
     sortkey = ''
     # Attribute names list, identifier attribute must be the first one and callled 'id'
-    att = ['_id','_name','_capacity','_type', '_operator.id']
+    att = ['_id','_name','_capacity','plant_type', '_operator.id']
     # Class header title
     header = 'Powerplant'
     # field description for use in, for example, input form
-    des = ['Id','Name','Capacity','type', 'Operator_Id']
+    des = ['Id','Name','Capacity','Plant_type', 'Operator_Id']
     # Constructor: Called when an object is instantiated
-    def __init__(self, id, name, capacity, type, operator_id):
+    def __init__(self, id, name, capacity, plant_type, operator_id):
         super().__init__()
         # Object attributes
         id = Powerplant.get_id(id)
         self._id = id
         self._name = str(name)
         self._capacity = int(capacity)
-        self._type = str(type)
+        self._plant_type = str(plant_type)
         self._operator_id = int(operator_id)
         # Add the new object to the dictionary of objects
         Powerplant.obj[id] = self
@@ -60,8 +60,8 @@ class Powerplant(Gclass):
         return self._salary
     
     @type.setter
-    def type(self, type):
-        self._type = type
+    def plant_type(self, plant_type):
+        self._plant_type = plant_type
         
     @property
     def operator_id(self):
